61b415cdcdd302623f5647a74024665bab585d8c
davidnedkonst <david.ned.kon@gmail.com>
2023-03-06 19:52:32 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 //Import
 
-import galleryItems from './gallery-items.js';
+import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
